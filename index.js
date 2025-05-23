fea13512380668504ac3cd45e742ce098f8e79c8
Axel Meyrek Mac <chavezsud1406@gmail.com>
2025-05-22 18:29:13 -0600

    Send message to Chat in Telegram

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* IMPORTACIONES */
 import puppeteer from "puppeteer";
+import 'dotenv/config';
 
 /* FUNCIONES */
 const validarVentaBoletos = async () =>  {
@@ -10,17 +11,37 @@ const validarVentaBoletos = async () =>  {
     const page = await navegador.newPage();
     await page.goto('https://leivaentradas.com');
 
-    /* proceso */
     const estadoDeBoletos = await page.evaluate(() => {
         const buttons = document.querySelectorAll('.btn');
         return buttons[63].textContent;
     });
 
-    if(estadoDeBoletos == 'Próximamente') console.log('Aun estan proximos');
-    else console.log('Los boletos ya estan a la venta');
+    if(estadoDeBoletos == 'Próximamente') {
+        console.log('Aun estan proximos');
+        enviarMensajeTelegram('Aun están proximos');
+    }
+    else {
+        console.log('Los boletos ya estan disponibles');
+        enviarMensajeTelegram('🎟 ¡Ya puedes comprar tus boletos de Leiva en https://leivaentradas.com!');
+    }
 
-    /* cierra navegador */
     await navegador.close();
 }
+
+const enviarMensajeTelegram = async (mensaje) => {
+  const token = process.env.TOKEN;
+  const chatId = process.env.CHATID;
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+  await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: mensaje,
+    }),
+  });
+};
+
 
 validarVentaBoletos();
